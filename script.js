ef2f52e0d5f26fec5b93fc63ea704edd2e5eaf53
QuirkeDev <sinuses_dab07@icloud.com>
2024-06-09 14:00:51 +0100

    Index.html update, beginning of JS animations

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
-    showPopup(true);
+function startAnimate() {
+  
+  document.getElementById("profile").style.marginLeft = "0%";
+  $("#main-char").animate( {
+    top: $(this).offset().top -27
+  }, 1000, function() {
+  });
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
-  }
+
 }
 
-function showPopup(bool) {
-  if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
-  } else {
-    document.getElementById('popup').style.visibility = 'hidden'
-  }
-}
+
+
+// Get the root element
+// var r = document.querySelector(':root');
+
+
+// / Create a function for setting a variable value
+// function myFunction_set() {
+//   // Set the value of variable --blue to another value (in this case "lightblue")
+//   r.style.setProperty('--blue', 'lightblue');
+// }
